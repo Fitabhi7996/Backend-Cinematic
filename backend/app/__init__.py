2f27d4a4431b961dacb94b13e8095e080adf90a0
unknown <ghodmareabhishek@gmail.com>
2023-07-24 12:42:03 +0530

    ok

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config["MONGO_URI"] = "mongodb+srv://ghodmareabhishek:5de4tAfbQ8drhQoS@cluster0.qa52dcw.mongodb.net/cinematic_event_management?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://ghodmareabhishek:5de4tAfbQ8drhQoS@cluster0.qa52dcw.mongodb.net/cinematic_event_management"
 mongo = PyMongo(app)
 
 
