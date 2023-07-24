da2ac61ae5f0dc671636ed3d0a2b34573f9109ba
unknown <ghodmareabhishek@gmail.com>
2023-07-24 14:31:26 +0530

    ok8

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-MONGO_URI = 'mongodb+srv://ghodmareabhishek:5de4tAfbQ8drhQoS@cluster0.qa52dcw.mongodb.net/cinematic_event_management?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://ghodmareabhishek:5de4tAfbQ8drhQoS@cluster0.qa52dcw.mongodb.net/cinematic_event_management'
 
 # mongodb+srv://ghodmareabhishek:<password>@cluster0.qa52dcw.mongodb.net/cinematic_event_management
 # mongodb+srv://ghodmareabhishek:5de4tAfbQ8drhQoS@cluster0.p2izv8b.mongodb.net/cinematic_event_management?retryWrites=true&w=majority
